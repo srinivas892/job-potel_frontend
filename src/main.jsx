615321d6f56a5fc2146c93cb107d763303b5e5ad
srinivas892 <chennasrinu555@gmail.com>
2025-07-15 19:54:16 +0530

    #

--- src/main.jsx
+++ src/main.jsx
@@ -15,10 +15,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
         <App />
         <Toaster />
-      </BrowserRouter>
     </PersistGate>
   </Provider>
 </React.StrictMode>,
